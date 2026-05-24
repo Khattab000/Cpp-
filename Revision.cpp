cfdfebaed4e1a1b167af473924ed5f13f0f4a7e6
Khattab <khattab.daud1@gmail.com>
2026-05-24 19:35:17 +0200

    Revision after chapter 2

---
+++ Revision.cpp
@@ -0,0 +1,32 @@
+#include <iostream>//Just a revision of everything we learned treat the code below as basically just a notepad
+//Last point we finished: Chapter 2 Quiz
+
+void doPrint()
+{
+	std::cout << "Print\n";
+}
+
+int main()
+{
+
+	int x{};
+
+	int y{};
+
+	std::cout << "Hello World!\n";
+
+	std::cout << "x = " << x << '\n';
+
+	std::cout << "Change x and then y: ";
+
+	std::cin >> x >> y;
+
+	std::cout << "x = " << x << '\n' << "y = " << y << '\n';
+
+	std::cout << sizeof(int) << '\n';
+
+	doPrint();
+
+	return 0;
+
+}
